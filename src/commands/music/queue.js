ded87fe465cf71b6796a2562528064124105aafa
Pierre-Alain BESSERO <pierre-alain.bessero@asteelflash.com>
2023-05-30 16:25:11 +0200

    Queue now works, needs cleanup

--- src/commands/music/queue.js
+++ src/commands/music/queue.js
@@ -1,4 +1,10 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  ButtonBuilder,
+  ActionRowBuilder,
+  ComponentType,
+} = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -18,18 +24,99 @@ module.exports = {
         ephemeral: true,
       });
     }
-    console.log(player.queue.all);
     const queue = player.queue.all;
     let page = 0;
-    let queuePart = queue.slice(0, 9);
+    let maxPage = Math.ceil(queue.length / 10);
+    let queuePart = queue.slice(0, 10);
+
+    const firstPage = new ButtonBuilder()
+      .setCustomId("firstPage")
+      .setLabel("<<")
+      .setStyle("Primary");
+
+    const previousPage = new ButtonBuilder()
+      .setCustomId("previousPage")
+      .setLabel("<-")
+      .setStyle("Primary");
+
+    const nextPage = new ButtonBuilder()
+      .setCustomId("nextPage")
+      .setLabel("->")
+      .setStyle("Primary");
+
+    const lastPage = new ButtonBuilder()
+      .setCustomId("lastPage")
+      .setLabel(">>")
+      .setStyle("Primary");
+
+    if (page === 0) {
+      // firstPage.setDisabled(true);
+      previousPage.setDisabled(true);
+    }
+
+    if (page + 1 === maxPage) {
+      nextPage.setDisabled(true);
+      // lastPage.setDisabled(true);
+    }
+
+    const row = new ActionRowBuilder().addComponents(
+      firstPage,
+      previousPage,
+      nextPage,
+      lastPage
+    );
 
     const queueEmbed = new EmbedBuilder()
-      .setTitle(`Queue (${page + 1} / ${~~(queue.size / 10)}`)
+      .setTitle(`Queue (${page + 1} / ${Math.ceil(queue.length / 10)})`)
       .setDescription(`Currently playing: **${player.current.title}**`)
       .setColor("#b734eb");
     for (const part of queuePart) {
       queueEmbed.addFields({ name: part.title, value: part.author });
     }
-    interaction.reply({ embeds: [queueEmbed] });
+    const response = await interaction.reply({
+      embeds: [queueEmbed],
+      components: [row],
+    });
+
+    const collector = response.createMessageComponentCollector({
+      componentType: ComponentType.Button,
+      time: 3_600_000,
+    });
+
+    collector.on("collect", async (i) => {
+      console.log(i);
+      const event = i.customId;
+      switch (event) {
+        case "firstPage":
+          page = 0;
+          break;
+        case "previousPage":
+          page -= 1;
+          break;
+        case "nextPage":
+          page += 1;
+          break;
+        case "lastPage":
+          page = maxPage - 1;
+          break;
+      }
+      // firstPage.setDisabled(page === 0);
+      previousPage.setDisabled(page === 0);
+      nextPage.setDisabled(page + 1 === maxPage);
+      lastPage.setDisabled(page + 1 === maxPage);
+
+      queueEmbed
+        .setFields()
+        .setTitle(`Queue (${page + 1} / ${Math.ceil(queue.length / 10)})`)
+        .setDescription(`Currently playing: **${player.current.title}**`);
+      queuePart = queue.slice(page * 10, (page + 1) * 10);
+      for (const part of queuePart) {
+        queueEmbed.addFields({ name: part.title, value: part.author });
+      }
+      await response.edit({
+        embeds: [queueEmbed],
+        components: [row],
+      });
+    });
   },
 };
