fe95b32489273bdb11097a936d631d90ff52d249
Pierre-Alain BESSERO <pierre-alain.bessero@asteelflash.com>
2023-05-30 15:19:14 +0200

    Started working on the queue Embed

--- src/commands/music/queue.js
+++ src/commands/music/queue.js
@@ -13,5 +13,17 @@ module.exports = {
       });
     }
     console.log(player.queue.all);
+    const queue = player.queue.all;
+    let page = 0;
+    let queuePart = queue.slice(0, 9);
+
+    const queueEmbed = new EmbedBuilder()
+      .setTitle(`Queue (${page + 1} / ${~~(queue.size / 10)}`)
+      .setDescription(`Currently playing: **${player.current.title}**`)
+      .setColor("#b734eb");
+    for (const part of queuePart) {
+      queueEmbed.addFields({ name: part.title, value: part.author });
+    }
+    interaction.reply({ embeds: [queueEmbed] });
   },
 };
